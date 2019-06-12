bdf324b9e6b3e1e3b4f8693459df31b98c5bf31c
Saad Manzur <smanzur@uci.edu>
2019-06-12 16:50:47 -0700

    foreground extraction

--- Background Subtraction/OpenCV Included/main.py
+++ Background Subtraction/OpenCV Included/main.py
@@ -1,4 +1,5 @@
 import cv2 as opencv
+import numpy as np
 
 
 def subtract_background(filename):
@@ -16,18 +17,37 @@ def subtract_background(filename):
     print('Frame width = ', video.get(opencv.CAP_PROP_FRAME_WIDTH))
     print('Frame height = ', video.get(opencv.CAP_PROP_FRAME_HEIGHT))
 
-    subtractor = opencv.createBackgroundSubtractorMOG2()
-    subtractor.setDetectShadows(False)
+    subtractor = opencv.createBackgroundSubtractorMOG2(detectShadows=False)
 
-    #print(subtractor.setHistory(100))
+    # print(subtractor.setHistory(100))
 
+    kernel = np.ones((9, 9), np.uint8)
+
+    rects = []
     while True:
         ret, frame = video.read()
 
         if frame is not None:
             foreground_mask = subtractor.apply(frame)
 
-            opencv.imshow('frame', foreground_mask)
+            foreground_mask = opencv.morphologyEx(foreground_mask, opencv.MORPH_OPEN, np.ones((3, 3), np.uint8))
+            foreground_mask = opencv.morphologyEx(foreground_mask, opencv.MORPH_CLOSE, kernel)
+
+            im2, contours, hierarchy = opencv.findContours(foreground_mask, mode=opencv.CHAIN_APPROX_SIMPLE,
+                                                           method=opencv.RETR_FLOODFILL)
+
+            frame_contours = []
+            for c in contours:
+                if opencv.contourArea(c) > 300:
+                    x, y, w, h = opencv.boundingRect(c)
+
+                    if h > w:
+                        frame_contours.append(np.array([x, y, w, h]))
+                        opencv.rectangle(frame, (x, y), (x+w, y+h), color=(255, 0, 0))
+
+            rects.append(frame_contours)
+
+            opencv.imshow('frame', frame)
 
             key = opencv.waitKey(30) & 0xff
             if key == 'q':
